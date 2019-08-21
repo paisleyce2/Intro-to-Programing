48c116a713d5593338d523593375b834d82dfd71
paisleyce2 <54373774+paisleyce2@users.noreply.github.com>
2019-08-21 14:34:46 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,26 @@
+/**********
+* Java Project Hello World
+*********
+* Lab 1
+* ____________
+* Claire Paisley
+* Aug 21, 2019
+* CMSC 255 Lab 1-2:50 W
+***********/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Claire Paisley");
+		System.out.println("Java Project Hello World");
+		System.out.println("Lab one, learning how to run Java on the computer and code a simple Hello World");
+		System.out.println("Version Mac OS X x64 version Kit 8 released April 16,2019");
+		System.out.println("CMSC 255 Lab W 1-2:50");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+
